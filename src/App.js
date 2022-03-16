0bda4a5849c1f852134d91014556d7e87bc6d233
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-16 20:29:27 +0000

    nav bar ugly, but showing up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 //import logo from './logo.svg';
+import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import './App.css';
 
-function App() {
+
+//the menu shouldn't link to another page, but should instead scroll the view down to see the menu interations.  Come back and check on this later
+//this will also need to be styled
+export default function App() {
   return (
-    <div className="App">
+    <div>
       <h1>React restaurant</h1>
       <nav style={{borderBottom: 'solid 1px', paddingBottom: '1rem',}}>
-        <Link to="/">Home</Link>| {" "}
-        <Link to="/src/routes/menu">Menu</Link>| {" "}
-        <Link to="/src/routes/visit">Visit Us</Link>| {" "}
-        <Link to="/src/routes/contact">Contact Us</Link>| {" "}
+        <Link to="www.google.com">Home</Link> | {" "}
+        <Link to="www.google.com">Menu</Link> | {" "}
+        <Link to="www.google.com">Contact</Link> | {" "}
+        <Link to="www.google.com">About</Link> | {" "}
       </nav>
       <Outlet />
     </div>
   );
 }
-
-export default App;
