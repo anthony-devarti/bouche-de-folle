4a19c98cd5f98219955a45aab1db67e0473086be
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-03-17 00:38:19 +0000

    trouble geting the logo in there

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 
 
+
 //the menu shouldn't link to another page, but should instead scroll the view down to see the menu interations.  Come back and check on this later
 //this will also need to be styled
 //why is it showing me that nav is undefined?  When I hover over it, it gives me the details as if 
@@ -15,7 +16,7 @@ export default function App() {
     <div>
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">React Restaurant</Navbar.Brand>
+          <Navbar.Brand href="#home"><img src="/workspace/bouche-de-folle/public/img/logo.png" alt="logo"></img></Navbar.Brand>
             <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#menu">Menu</Nav.Link>
